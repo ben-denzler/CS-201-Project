006e96fe7e54f8a5004997c3ad0c09078e747f44
Benjamin Denzler <bdenz001@ucr.edu>
2023-11-05 13:31:27 -0800

    Completed step 3

--- Pass/HelloPass/HelloPass.cpp
+++ Pass/HelloPass/HelloPass.cpp
@@ -18,42 +18,50 @@ struct HelloPass : public FunctionPass {
     static char ID;
     HelloPass() : FunctionPass(ID) {}
 
+    // Gets called for each function in code
     bool runOnFunction(Function &F) override {
         errs() << "HelloPass runOnFunction: ";
         errs() << F.getName() << "\n";
 
+        int numBlocks = 0;
+
+        // Iterates over basic blocks of the function
         for (auto &basic_block : F) {
+            errs() << "\nBlock #" << numBlocks++ << "\n";
+            
+            int numPred = 0;
+            int numSucc = 0;
             for (auto *pred: predecessors(&basic_block)) {
+                numPred++;
             }
             for (auto *succ: successors(&basic_block)) {
+                numSucc++;
             }
+            errs() << "Predecessors: " << numPred << "\n";
+            errs() << "Successors: " << numSucc << "\n";
+
+            // Iterates over instructions in a basic block
             for (auto &inst : basic_block) {
                 errs() << inst << "\n";
                 if (inst.getOpcode() == Instruction::Load) {
-                    errs() << "This is Load"
-                           << "\n";
+                    errs() << "This is Load" << "\n";
                 }
                 if (inst.getOpcode() == Instruction::Store) {
-                    errs() << "This is Store"
-                           << "\n";
+                    errs() << "This is Store" << "\n";
                 }
                 if (inst.isBinaryOp()) {
                     errs() << "Op Code:" << inst.getOpcodeName() << "\n";
                     if (inst.getOpcode() == Instruction::Add) {
-                        errs() << "This is Addition"
-                               << "\n";
+                        errs() << "This is Addition" << "\n";
                     }
                     if (inst.getOpcode() == Instruction::Sub) {
-                        errs() << "This is Subtraction"
-                               << "\n";
+                        errs() << "This is Subtraction" << "\n";
                     }
                     if (inst.getOpcode() == Instruction::Mul) {
-                        errs() << "This is Multiplication"
-                               << "\n";
+                        errs() << "This is Multiplication" << "\n";
                     }
                     if (inst.getOpcode() == Instruction::SDiv) {
-                        errs() << "This is Division"
-                               << "\n";
+                        errs() << "This is Division" << "\n";
                     }
 
                     // See Other classes, Instruction::Sub, Instruction::UDiv,
